fc90e0ec9223dda0df0235ccecfa33e8eae7438b
Nicole <98623124+rabbitcache@users.noreply.github.com>
2025-02-20 16:39:43 -0600

    Update SubscriptionShepardController.java

--- SubscriptionShepardController.java
+++ SubscriptionShepardController.java
@@ -1,11 +1,9 @@
-package edu.metro.iteration2;
+package edu.metro.subscriptionshepard;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class SubscriptionShepardController {
@@ -28,6 +26,23 @@ public class SubscriptionShepardController {
         return "redirect:/dashboard";
     }
 
+    @GetMapping("/retrieve/{id}")
+    public String retrieveAll(@PathVariable int id, Model model){
+        model.addAttribute("subscription", subService.retrieve(id));
+        return "/subscription";
+    }
 
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable int id, Model model){
+        subService.delete(id);
+        return "redirect:/dashboard";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute Subscription sub, Model model){
+        subService.update(sub);
+        return "redirect:/dashboard";
+    }
 
 }
+
