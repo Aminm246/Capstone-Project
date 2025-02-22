8e26964fc50de801f58f0eed248a274f22d10a6a
Nicole <98623124+rabbitcache@users.noreply.github.com>
2025-02-22 14:22:03 -0600

    Add files via upload

---
+++ src/main/java/edu/metro/subscriptionshepard/SubscriptionshepardApplication.java
@@ -0,0 +1,13 @@
+package edu.metro.subscriptionshepard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionshepardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriptionshepardApplication.class, args);
+    }
+
+}
